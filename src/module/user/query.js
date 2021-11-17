d420843cbad62d19f5702d8f44ff2865963e70a4
Dpk1999 <deepak.gaikwad@successive.tech>
2021-11-17 15:30:23 +0530

    feat:bootstrapping apollo server and queries

---
+++ src/module/user/query.js
@@ -0,0 +1,9 @@
+export default { 
+    getMyProfile: () => {
+        return {
+            id: 1,
+            name:'Deepak',
+            email:'deepak.gaikwad@successive.tech',
+        }
+    },
+}
