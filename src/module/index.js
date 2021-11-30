42fc50fc2522be4fee73cd88aa07ba72588d0480
Dpk1999 <deepak.gaikwad@successive.tech>
2021-11-30 10:29:11 +0530

    feat:data sources

--- src/module/index.js
+++ src/module/index.js
@@ -4,8 +4,10 @@ import * as trainee from './trainee';
 import * as user from './user';
 
 const typesArray = fileLoader(path.join(__dirname, './**/*.graphql'));
-
+console.log('user', user);
 const typeDefs = mergeTypes(typesArray, { all: true });
+console.log('typeDefs ---->', typeDefs);
+console.log('user.getMyProfile ---->', user);
 
 export default {
   resolvers: {
@@ -14,10 +16,12 @@ export default {
       ...trainee.Query,
     },
     Mutation: {
+      ...user.Mutation,
       ...trainee.Mutation,
     },
     Subscription: {
       ...trainee.Subscription,
+      ...user.Subscription,
     },
   },
   typeDefs,
